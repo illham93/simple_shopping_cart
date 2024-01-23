5f6c75856f5ef4f2eeaf921a35a033d599ebbc83
Devon <devonhamill93@gmail.com>
2024-01-23 15:37:57 -0500

    V1.0

--- js/application.js
+++ js/application.js
@@ -3,11 +3,13 @@ var updateTotal = function() {
     $('tbody tr').each(function (i, element) {
         var price = parseFloat($(element).children('.price').children('span').text());
         var quantity = parseFloat($(element).find('.quantity input').val());
-        var total = Math.round(price * quantity * 100)/100;
-        $(element).children('.total').html("$" + total)
+        var total = (Math.round(price * quantity * 100)/100);
         grandTotal += total;
+        total = total.toFixed(2);
+        $(element).children('.total').html("$" + total)
+   
     });
-    grandTotal = Math.round(grandTotal*100)/100;
+    grandTotal = (Math.round(grandTotal*100)/100).toFixed(2);
     $('#grandTotal').html(grandTotal);
 }
 
@@ -24,7 +26,7 @@ $(document).ready(function() {
         }, 250);
     })
 
-    // Make X button delete a row
+    // Make X button delete a row and update total
     $(document).on('click', '.remove', function() {
         $(this).closest('tr').remove();
         updateTotal();
@@ -34,11 +36,31 @@ $(document).ready(function() {
     $('#addItem').on('submit', function(event) {
         event.preventDefault();
         var name = $(this).children('[name=name]').val();
-        var price = $(this).children('[name=price]').val();
+        var price = Number($(this).children('[name=price]').val()).toFixed(2);
         var quantity = $(this).children('[name=quantity]').val();
-        var total = Math.round(price * quantity * 100)/100;
+        var total = (Math.round(price * quantity * 100)/100).toFixed(2);
 
-        $('tbody').append(
+        $('.table1').append(
+            '<tr>' + 
+                '<td class="item">' + name + '</td>' +
+                '<td class="price">$<span>' + price + '</span></td>' +
+                '<td class="quantity"><input type="number" value="' + quantity + '"></td>' +
+                '<td class="total">$' + total + '</td>' +
+                '<td><button class="btn btn-sm btn-success add">✓</button></td>' +
+                '<td><button class="btn btn-sm btn-danger remove">X</button></td>' + 
+            '</tr>'
+        );
+        updateTotal();
+    });
+
+    // Add to cart button
+    $(document).on('click', '.add', function() {
+        var name = $(this).parent().siblings('.item').text();
+        var price = Number($(this).parent().siblings('.price').text().slice(1)).toFixed(2);
+        var quantity = Number($(this).parent().siblings('.quantity').children().val());
+        var total = (Math.round(price * quantity * 100)/100).toFixed(2);
+        $(this).closest('tr').remove();
+        $('.table2').append(
             '<tr>' + 
                 '<td class="item">' + name + '</td>' +
                 '<td class="price">$<span>' + price + '</span></td>' +
@@ -47,6 +69,5 @@ $(document).ready(function() {
                 '<td><button class="btn btn-sm btn-danger remove">X</button></td>' + 
             '</tr>'
         );
-        updateTotal();
-    });
+    })
 });
